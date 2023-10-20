e80686a812def23e6e7aa4d75fef15671d7f25ef
Anurag <anuragyadav41682@gmail.com>
2023-10-20 16:42:19 +0530

    Added more Project and updated Readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # My Portfolio Website
+
 Made using React !
+
 ## Deployment
-[See Live](https://anur4g.netlify.app/)
+
+https://anur4g.netlify.app/
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,32 +1,44 @@
 import ProjectsCard from "./ProjectsCard.jsx";
 function Projects() {
-	return (
-		<div id="Projects" className="Projects">
-			<ProjectsCard
-				name="E-Learning"
-				img="./Images/elearning.png"
-				stack={["HTML", "Javascript", "CSS", "React"]}
-				link="https://e-learning-website-1.hop.sh/"
-			></ProjectsCard>
-			<ProjectsCard
-				name="My-Portfolio"
-				img="./Images/Portfolio.png"
-				stack={["HTML", "Javascript", "CSS", "React"]}
-				link="https://anur4g.netlify.app/"
-			></ProjectsCard>
-			<ProjectsCard
-				name="To-Do WebApp"
-				img="./Images/todo.png"
-				stack={["EJS", "Javascript", "CSS", "HTML"]}
-				link="https://to-do-list-8310.onrender.com/"
-			></ProjectsCard>
-			<ProjectsCard
-				name="Guessing Game"
-				img="./Images/guess.png"
-				stack={["HTML", "CSS", "Javascript"]}
-				link="https://anurag41682.github.io/Guessing-Game/"
-			></ProjectsCard>
-		</div>
-	);
+  return (
+    <div id="Projects" className="Projects">
+      <ProjectsCard
+        name="Job-Tracker"
+        img="/Images/jobTracker"
+        stack={["Javascript", "Express", "Material UI", "Passport.js"]}
+        link="https://job-tracker-beta.vercel.app/login"
+      ></ProjectsCard>
+      <ProjectsCard
+        name="Memories WebApp"
+        img="/Images/memories.png"
+        stack={["HTML", "Javascript", "CSS", "React", "Express"]}
+        link="https://memories-web-app-two.vercel.app/"
+      ></ProjectsCard>
+      <ProjectsCard
+        name="E-Learning"
+        img="./Images/elearning.png"
+        stack={["HTML", "Javascript", "CSS", "React"]}
+        link="https://e-li8rary.netlify.app//"
+      ></ProjectsCard>
+      <ProjectsCard
+        name="My-Portfolio"
+        img="./Images/Portfolio.png"
+        stack={["HTML", "Javascript", "CSS", "React"]}
+        link="https://anur4g.netlify.app/"
+      ></ProjectsCard>
+      <ProjectsCard
+        name="To-Do WebApp"
+        img="./Images/todo.png"
+        stack={["EJS", "Javascript", "CSS", "HTML", "Express"]}
+        link="https://to-do-list-bda5.onrender.com/"
+      ></ProjectsCard>
+      <ProjectsCard
+        name="Guessing Game"
+        img="./Images/guess.png"
+        stack={["HTML", "CSS", "Javascript"]}
+        link="https://anurag41682.github.io/Guessing-Game/"
+      ></ProjectsCard>
+    </div>
+  );
 }
 export default Projects;
