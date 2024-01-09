7d140d6a6890d12195a49bb7542c6283862347c2
anurag41682 <anuragyadav41682@gmail.com>
2024-01-09 20:06:53 +0530

    Added More Skills

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -5,11 +5,11 @@ function Achievements() {
         <li>
           Got the global rank of 2247 in google's kickstart 2022 (Round B).
         </li>
-        <li>Pupil at Codeforces.</li>
-        <li>3 Star at Codechef.</li>
-        <li>In top 10% on Leetcode rating wise. </li>
-        <li>Contributor GSSoC 2023.</li>
-        <li>Solved above 1000 problems on various coding platforms.</li>
+        <li>Pupil on Codeforces.</li>
+        <li>3 Star on Codechef.</li>
+        <li>Rated 1780+ on Leetcode.</li>
+        <li>In top 8% on Leetcode rating wise. </li>
+        <li>Solved over 1000 problems on various coding platforms.</li>
       </ol>
     </div>
   );
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,34 +1,36 @@
 function Skills() {
-	return (
-		<div id="skills" className="skill">
-			<h2>Languages :</h2>
-			<div className="Skills-Language">
-				<img className="skill-img" src="./Images/c++.png" />
-				<img className="skill-img" src="./Images/Javascript.png" />
-				<img className="skill-img" src="./Images/html.png" />
-				<img className="skill-img" src="./Images/css.png" />
-			</div>
-			<h2>Database :</h2>
-			<div className="Skills-Database">
-				<img className="skill-img" src="./Images/mongodb.png"></img>
-			</div>
-			<h2>Framework :</h2>
-			<div className="Skills-FrameWork">
-				<img className="skill-img" src="./Images/express.png"></img>
-			</div>
-			<h2>Library :</h2>
-			<div className="Skills-Libraries">
-				<img className="skill-img" src="./Images/react.png"></img>
-			</div>
-			<h2>Others :</h2>
-			<div className="Skills-Others">
-				<img className="skill-img" src="./Images/node.png"></img>
-				<img className="skill-img" src="./Images/git.png" />
-				<img className="skill-img" src="./Images/github.png" />
-				<img className="skill-img" src="./Images/vscode.png"></img>
-				<img className="skill-img" src="./Images/vim.png"></img>
-			</div>
-		</div>
-	);
+  return (
+    <div id="skills" className="skill">
+      <h2>Languages</h2>
+      <div className="Skills-Language">
+        <img className="skill-img" src="./Images/c++.png" />
+        <img className="skill-img" src="./Images/Javascript.png" />
+        <img className="skill-img" src="./Images/html.png" />
+        <img className="skill-img" src="./Images/css.png" />
+      </div>
+      <h2>Databases</h2>
+      <div className="Skills-Database">
+        <img className="skill-img" src="./Images/mongodb.png"></img>
+        <img className="skill-img" src="./Images/mysql.png"></img>
+      </div>
+      <h2>Framework</h2>
+      <div className="Skills-FrameWork">
+        <img className="skill-img" src="./Images/express.png"></img>
+      </div>
+      <h2>Libraries</h2>
+      <div className="Skills-Libraries">
+        <img className="skill-img" src="./Images/react.png"></img>
+        <img className="skill-img" src="./Images/material-ui.png"></img>
+      </div>
+      <h2>Others</h2>
+      <div className="Skills-Others">
+        <img className="skill-img" src="./Images/node.png"></img>
+        <img className="skill-img" src="./Images/git.png" />
+        <img className="skill-img" src="./Images/github.png" />
+        <img className="skill-img" src="./Images/vscode.png"></img>
+        <img className="skill-img" src="./Images/vim.png"></img>
+      </div>
+    </div>
+  );
 }
 export default Skills;
