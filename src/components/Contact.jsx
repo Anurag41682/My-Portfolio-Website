43275b2fe4a34e0f654b498412aecdc0375a3331
Anurag41682 <anuragyadav41682@gmail.com>
2024-06-20 18:46:27 +0530

    Updated X link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,20 @@
 import ContactItems from "./ContactItems";
 function Contact() {
-	return (
-		<div className="Contact">
-			<ContactItems
-				img="./Images/github.svg"
-				lnk="https://github.com/Anurag41682"
-			></ContactItems>
-			<ContactItems
-				img="./Images/linkedin.svg"
-				lnk="https://www.linkedin.com/in/anurag41682/"
-			></ContactItems>
-			<ContactItems
-				img="./Images/Twitter.svg"
-				lnk="https://twitter.com/Anurag_2k2"
-			></ContactItems>
-		</div>
-	);
+  return (
+    <div className="Contact">
+      <ContactItems
+        img="./Images/github.svg"
+        lnk="https://github.com/Anurag41682"
+      ></ContactItems>
+      <ContactItems
+        img="./Images/linkedin.svg"
+        lnk="https://www.linkedin.com/in/anurag41682/"
+      ></ContactItems>
+      <ContactItems
+        img="./Images/Twitter.svg"
+        lnk="https://x.com/Anur4g_Yadav"
+      ></ContactItems>
+    </div>
+  );
 }
 export default Contact;
